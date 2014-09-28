e3d64cb4b4e5fccaa3f020f6de6e548deff63e9d
Mason <mason.mideumlee@gmail.com>
2014-09-27 20:54:04 -0700

    Ask Question

--- js/hsv-to-rgb.js
+++ js/hsv-to-rgb.js
@@ -2,28 +2,66 @@ function hsv2rgb(colour) {
 	var h = (colour[0] / 60) % 6;
 	var s = colour[1] / 255;
 	var v = colour[2] / 100;
-	    
+		
 	if (h < 0) { h = h + 6; }
 
-     // The reason why r, g, b = v, is because if h is undefined
-     // r, g, b = v is the default fallback. See documentations on
-     // HSV to RGB conversion.
-     var r = v; var g = v; var b = v;
+	// The reason why r, g, b = v, is because if h is undefined
+	// r, g, b = v is the default fallback. See documentations on
+	// HSV to RGB conversion.
+	var r = v,
+		g = v,
+		b = v;
 
-     var i = Math.floor(h * 6);
-     var f = h * 6 - i;
-     var p = v * (1 - s);
-     var q = v * (1 - f * s);
-     var t = v * (1 - (1 - f) * s);
-
-      switch(i % 6) {
-		case 0: r = v, g = t, b = p; break;
-		case 1: r = q, g = v, b = p; break;
-		case 2: r = p, g = v, b = t; break;
-		case 3: r = p, g = q, b = v; break;
-		case 4: r = t, g = p, b = v; break;
-		case 5: r = v, g = p, b = q; break;
-      }
-    	
-    	return [Math.floor(r*255), Math.floor(g*255), Math.floor(b*255)];
+	// Temporary variable for calculation
+	var temp1  = v * (1-s),
+		temp2 = v * (1-(h-Math.abs(h))*s),
+		temp3 = v * (1-(1-(h-Math.abs(h)))*s);
+    
+	if(h>=0 && h<1) {
+		r=v; g=temp3; b=temp1;
+	}
+	else if(h>=1 && h<2) {
+		r=temp2; g=v; b=temp1;
+	}
+	else if(h>=2 && h<3) {
+		r=temp1; g=v; b=temp3;
+	}
+	else if(h>=3 && h<4) {
+		r=temp1; g=temp2; b=v;
+	}
+	else if(h>=4 && h<5) {
+		r=temp3; g=temp1; b=v;
+	}
+	else if(h>=5 && h<6) {
+		r=v; g=temp1; b=temp2;
+	}
+    
+	// Convert r, g, b to 8-bit by multiplying them with 255
+	return [Math.floor(r*255), Math.floor(g*255), Math.floor(b*255)];
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
